4dd9eed93a8c8476e102855ce09d18f8678e1f85
Steve <ostylehouse@gmail.com>
2025-07-05 02:44:34 +1200

    working prod (only) this diverges from main, which has the dev server

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
-const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+export default {
+	kit: {
+		adapter: adapter()
+	}
 };
-
-export default config;
