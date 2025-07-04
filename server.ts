40f5d856b214d278b9fa3305a6666329d47be11c
Steve <ostylehouse@gmail.com>
2025-07-05 03:10:00 +1200

    prod works

---
+++ server.ts
@@ -0,0 +1,28 @@
+import { Server } from 'socket.io';
+import { handler } from './build/handler.js';
+import { init_an_io } from './src/lib/ws-server/ws-server';
+import express from 'express';
+import { createServer } from 'http';
+
+const app = express();
+const server = createServer(app);
+
+// Initialize Socket.IO
+const io = new Server(server);
+
+init_an_io(io)
+
+// requests for eg /_app/immutable/entry/app.6lm5gro6.js
+// which are in this directory
+app.use(express.static('build/client'));
+// Let SvelteKit handle everything else..?
+// will come up with something to be 'GET /'
+//  leading to the above (they have to be in this order)
+app.use(handler);
+
+const port = process.env.PORT || 19091;
+const host = process.env.HOST || '0.0.0.0';
+
+server.listen(port, host, () => {
+  console.log(`socket.io server running on http://${host}:${port}`);
+});
