e07d40629cde8fb86d21a66dfc34627fa2ecd0ee
Steve <ostylehouse@gmail.com>
2025-11-29 17:43:19 +1300

    move Dierarchy etc to Selection

--- src/lib/mostly/Selection.svelte.ts
+++ src/lib/mostly/Selection.svelte.ts
@@ -182,6 +182,7 @@ export class Travel extends TheC {
 
 // maintain a mirroring of n** -> D**
 //  responding to changes in source n**
+// a Se is TheC, only extends Travel for this .c+ = opt thing, doesn't .dive() itself...
 export class Selection extends Travel {
     constructor(opt={}) {
         // .c += opt
@@ -319,6 +320,428 @@ export class Selection extends Travel {
     }
     async reverse(y) {
         return await this.c.T.reverse(y)
+    }
+
+}
+
+//#endregion
+//#region Dierarchy
+
+function Tdebug(T,title,say,etc) {
+    let indent = T.c.path.map(T=>'  ').join('')
+    let D = T.sc.D
+    console.log(`${title} ${indent} ${D.sc.name}\t${say||''}`,...[etc].filter(n=>n != null))
+}
+
+class Dierarchy {
+    // < keeping things around
+    // < findable orphaned D** via path (fragments) and filesizes
+    D_to_name(D) {
+        return D.sc.name
+    }
+    D_to_path(D) {
+        let path = D.c.T.c.path
+        return path.map(T => this.D_to_name(T.sc.D))
+    }
+    i_path(D,j) {
+        let i = 0
+        for (let bit of this.D_to_path(D)) {
+            j.i({path:bit,seq: i++})
+        }
+    }
+    // does the journey flow into here
+    async journeys_choose_D(T:Travel,oT:Travel) {
+        let D = T.sc.D
+        let seq = oT.c.path.length-1
+        let oD = oT.sc.D
+        let bit = this.D_to_name(oD)
+        
+        await oD.replace({journey:1},async () => {
+            for (let j of (T.sc.journeys||[])) {
+                // if we're currently within the parameters of the tour
+                //  so it should flood into yonders
+                let goes = D.o({plodding:j,tour:1})[0]
+                if (goes && goes.sc.ends) goes = null
+
+                // < seq=1 matches everything! what do we do about that...
+                //    use an exotic unicode character in the code
+                //     obscured by LieSurgery
+                let pathbit = j.o({path:1,seq}).filter(n=>n.sc.seq == seq)[0]
+
+                if (goes || pathbit) {
+                    // Tdebug(T,'journey goesto',`journey: ${keyser(j)} seq:${seq}`,pathbit)
+                    // wants to go here
+                    oT.sc.journeys ||= []
+                    oT.sc.journeys.push(j)
+                }
+            }
+        })
+    }
+    // T%journeys/$j and D**%plodding go inward
+    async journeys_affect_D(T:Travel) {
+        let uD = T.up?.sc.D
+        let D = T.sc.D
+        // all %plodding copy down, noting if they're opening, ending etc
+        let openness_suggestions:Array<number> = []
+        let journey_gives_up:Array<TheC> = []
+        for (let j of (T.sc.journeys||[])) {
+            // have moved, using a D** ephemeral
+            // gather plodding information
+            await D.replace({plodding:1},async () => {
+                // Tdebug(T,'journey affect',`journey: ${keyser(j)}`)
+                let pls:TheN = uD?.oa({plodding:j})
+
+                if (!pls) {
+                    if (T != T.c.top) throw "plodding T!top"
+                    // when there is nothing,
+                    //  establish a shared energy budget for all D**
+                    D.i({plodding:j,enthusiasmus:{journey:0,energy:5}})
+                    // D** starts being inside the journey
+                    D.i({plodding:j,tour:{DN:[D]}})
+                }
+                else {
+                    for (let pl of pls) {
+                        // whole lot of protocols carrying themselves into D**
+                        let to = pl.sc.tour
+                        if (to) {
+                            if (to.ends) {
+                                // don't do any more %%plodding here
+                                openness_suggestions.push(2)
+                                return
+                            }
+                            // tour += D
+                            to.DN.push(D)
+                            // < is this where to check where the journey ends?
+                        }
+                    }
+                    for (let pl of pls) {
+                        let en = pl.sc.enthusiasmus
+                        if (en) {
+                            // simulates going further
+                            en.journey += 1
+                            
+                            if (en.energy == en.journey) {
+                                // will be first next.
+                                journey_gives_up.push(j)
+                            }
+                            if (en.energy <= en.journey) {
+                                // from here on
+                                openness_suggestions.push(2)
+                            }
+                            else {
+                                openness_suggestions.push(3)
+                            }
+                        }
+                        // copy into D**
+                        D.i(pl.sc)
+                    }
+                }
+            })
+
+            for (let j of journey_gives_up) {
+                // in addition to suggesting low-openness
+                await this.journey_gives_up(j,T)
+            }
+
+            let most_awake = openness_suggestions.sort().pop()
+            if (T != T.c.top && !most_awake) throw "there should be at least 1 %journey"
+            await this.i_openity(D,most_awake)
+        }
+    }
+    async stop_the_tour(j,T) {
+        let D = T.sc.D
+        let to = D.o({plodding:j,tour:1})[0]
+        if (!to) throw "cancel!to"
+        to.ends = D
+        // < later siblings of D
+    }
+    async journey_gives_up(j,T) {
+        let topD = T.c.top.sc.D
+        let D = T.sc.D
+        // note in %journey/%gaveup where it ends,
+        //  ~~ %journey/%ends at a non-meaningful point
+        //  which could become the next %journey,begins
+        this.stop_the_tour(j,T)
+        await j.replace({gaveup:1}, async () => {
+            // ~~ %journey/%path
+            let j = topD.i({gaveup:1})
+            this.i_path(D,j)
+        })
+    }
+
+
+    async i_openity(D,openity:number) {
+        await D.replace({openity:1},async () => {
+            openity && D.i({openity})
+        })
+    }
+
+}
+//#endregion
+//#region DirectorySelectivityUtils
+type TheD = TheC
+export class DirectorySelectivityUtils extends Dierarchy {
+    M:Modus
+    constructor (M) {
+        super()
+        this.M = M
+    }
+    get thetime() {
+        return this.M.thetime
+    }
+    async resolved_nibs(T:Travel,N:Array<Travel>,goners:Array<TheD>) {
+        for (let oT of N) {
+            await this.journeys_choose_D(T,oT)
+            await this.journeys_affect_D(oT)
+        }
+
+        for (let go of goners) {
+            // < keep their D** hanging around, subsiding...
+        }
+    }
+    // for n%nib:dir only
+    async possibly_expand_nib(T:Travel) {
+        let {D,bD,n} = T.sc
+        let time = this.thetime
+        let topD = T.c.top.sc.D
+
+        if (!T.sc.frontier) {
+            if (T == T.c.top) {
+                // where we aim to start doing it
+                if (!topD.oa({journey:1})) {
+                    // start with a journey that begins immediately
+                    //  it wanders a while then gets tired
+                    let j = topD.i({journey:'auto',begins:1})
+                    this.i_path(D,j)
+                }
+                T.sc.journeys = topD.o({journey:1})
+                // you might address the top of the %Tree
+                await this.journeys_affect_D(T)
+            }
+            else {
+                let journeys = T.sc.journeys
+                // < %journeys already aimed by T.up at resolved_fn
+            }
+        }
+
+        D.i({frontierity:"IS",time})
+
+
+
+
+
+        let openity = D.o({openity:1},1)[0] || 3
+        if (openity <3) {
+            return T.sc.not = 'chilling'
+        }
+
+        // < sensible timings of:
+        await this.expand_nib(n)
+        // this.collapse_nib(n)
+    }
+
+    
+    async expand_nib(n:TheC) {
+        const DL:DirectoryListing = n.sc.DL
+        await DL.expand()
+
+        // i /*%nib:dir,...
+        let uDL = DL
+        await n.replace({nib:1,name:1},async () => {
+            for (const DL of uDL.directories) {
+                let di = n.i({nib:'dir',name:DL.name,DL,thetime:this.thetime})
+            }
+            for (const FL of uDL.files) {
+                n.i({nib:'blob',name:FL.name,FL,thetime:this.thetime})
+            }
+        })
+    }
+    async collapse_nib(n:TheC) {
+        const DL:DirectoryListing = n.sc.DL
+        DL.collapse()
+        // forget $n/**
+        await n.replace({nib:1,name:1},async () => {
+        })
+    }
+
+
+    // guessing, implying structure
+    // upwards, propagate D**%readin=$aspect about the tracks
+    // Artist get %readin:type,val:artist
+    async intelligible_name(T:Travel) {
+        let {D,n} = T.sc
+        let i_readin = (t:string,s:TheUniversal) => {
+            D.i({readin: t, thetime: this.thetime, ...s})
+        }
+        await D.replace({readin: 1}, async () => {
+            const name = n.sc.name as string;
+            const p = new Parserify(name);
+            
+            if (n.sc.nib === 'blob') {
+                let format, type, seq
+
+                // Extract and index file extension
+                if (p.p(/\.([a-z0-9]{3,4})$/i)) {
+                    format = p[1]!.toLowerCase();
+                    i_readin('name', {format});
+                    
+                    // Determine if it's a track
+                    if (format.match(/^(mp3|wav|ogg|m4a|aac|flac|opus|webm|oga|mkv|mka|mp4)$/)) {
+                        type = 'track'
+                        i_readin('name',{type});
+                    }
+                }
+                
+                // Extract and index track number from start
+                if (p.p(/^(\d+)[\s.\-_]+/, '')) {
+                    seq = parseInt(p[1]!, 10);
+                    i_readin('name', {seq});
+                }
+                
+                // Clean up punctuation noise at the edges
+                p.p(/^[\s\-_\.]+/)
+                p.p(/[\s\-_\.]+$/);
+                // Collapse multiple spaces
+                while (p.p(/  +/,' ')) 1
+                
+                // Final cleaned name
+                const cleaned_name = p.s.trim();
+                i_readin('name', {name: cleaned_name});
+
+                if (type == 'track') {
+                    // coherent enough!
+                    //  make %%art out of all this
+                    //   unlikely to be wrong at this level,
+                    //    we may regard more context when hoisting it as %%ads
+                    i_readin('art',{track:cleaned_name,seq,format})
+                }
+            }
+            
+            if (n.sc.nib === 'dir') {
+                // Directory handling depends on finding D**%readin:type,val:track somewhere
+                //  extrapolate outwards to become D^^%readin:type,val:artist
+                // note D.replace() is happening, so we .bo() the previous time
+                //  during replace(), only the newly placed items are findable with .o()
+                
+                let said_album = 0
+                let said_artist = 0
+                let said_collection = 0
+                for (let oD of D.bo({Tree:3})) {
+                    // D:Album/Tree:Track*
+                    if (oD.oa({readin:1,type:'track'})) {
+                        // tracks imply we are an album
+                        if (!said_album++) {
+                            i_readin('inners',{type:'album'})
+                        }
+                    }
+                    if (oD.oa({readin:1,type:'album'})) {
+                        // albums imply we are an artist
+                        if (!said_artist++) {
+                            i_readin('inners',{type:'artist'})
+                        }
+                    }
+                    if (oD.oa({readin:1,type:'artist'})) {
+                        // artists imply we are a collection
+                        if (!said_collection++) {
+                            i_readin('inners',{type:'collection'})
+                        }
+                    }
+                }
+
+                // < be more tentative, guess, configure what character
+                // notice '0 chill' etc genre|classification|purpose directories
+                if (p.p(/^(0|-) /i)) {
+                    if (!said_collection++) {
+                        i_readin('inners',{type:'collection'})
+                    }
+                }
+
+                // 
+                if (said_album && said_artist
+                    || (said_album || said_artist) && said_collection) {
+                    // an artist dir with a few loose tracks?
+                    D.o({readin:1,type:'album'}).map(n => D.drop(n))
+                    said_collection
+                        && D.o({readin:1,type:'artist'}).map(n => D.drop(n))
+                }
+
+
+                i_readin('name', {name: p.s.trim(), thetime: this.thetime});
+            }
+        });
+    }
+
+    // upwards propagate more stuff
+    async percolating_ads(T:Travel) {
+        let {D} = T.sc
+        // random flood of %ads hoisted about tracks
+        // < do in a second traversal of D** and incorporate how the hierarchies have flopped out
+        //    including from top-down
+        //    
+        // < which pseudo-randomly shuffles what to talk about at the top level
+        // 
+        // < can make ads about:
+        //    D/oD/%readin=type,val=track|album|artist
+
+        //    D/oD/%readin=track
+        //    D/oD/%readin=album
+        //    D/oD/%readin=artist
+        // the fact we also D%readin=
+
+        // < prefers to advertise albums
+
+        // < guess if we're "Artist - Album (1979)" based on the surroundings
+        // haphazardly extend up, or spawn whole, %artist,album,track tuples
+        await D.replace({ads: 1}, async () => {
+            // look at here
+            let meta = {}
+            let gather_meta = (type:string) => {
+                let name = D.boa({type:type,readin:1})
+                        && D.boa({name:1,   readin:'name'},1)[0]
+                if (name != null) {
+                    meta[type] = name
+                }
+            }
+            gather_meta('collection')
+            gather_meta('artist')
+            gather_meta('album')
+            gather_meta('track')
+
+            // meta accumulates when hoisted, on ads
+            let i_ads = (t:string,sc={}) => {
+                // set ads twice for object property ordering...
+                //   because sc may contain it from below
+                //  ie every %thing,with,values is a thing primarily
+                return D.i({ads:0,...meta,...sc,ads:t,
+                     thetime:this.thetime})
+            }
+
+            let some_inners = false
+            for (let oD of D.bo({Tree:3})) {
+                // ads build up locality as they hoist
+                let few = 3
+                oD.oa({ads:1})?.map((n:TheC) => {
+                    if (few-- < 1) return
+                    i_ads('beyond',n.sc)
+                    some_inners = true
+                })
+            }
+
+            if (hak(meta)) {
+                // we could start advertising this thing!
+                if ('prand' || 'track') {
+                    // starts with|as any %%art found here
+                    //  so individual track info as such
+                    // < folder.jpg?
+                    let arts = D.boa({readin:'art'})?.map((n:TheC) => ({...n.sc}))
+                        || [{noart:1}]
+                    for (let art of arts) {
+                        delete art.readin
+                        i_ads('here',art)
+                    }
+                }
+            }
+        });
     }
 
 }
