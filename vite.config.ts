3aedaf6a2bb4920bea6fd330951c7dffd857dced
Steve <ostylehouse@gmail.com>
2026-02-04 21:04:00 +1300

    or

--- vite.config.ts
+++ vite.config.ts
@@ -13,18 +13,6 @@ export default defineConfig({
     sourcemap: true, 
     // 3. Keep code modern to avoid transpilation "noise"
     target: 'esnext', 
-    rollupOptions: {
-      input: 'server.ts',
-      external: ['express', 'socket.io'],
-      output: {
-        // 4. THIS is the "Less Compiley" secret: 
-        // It outputs one file per source file instead of one big bundle.
-        preserveModules: true,
-        preserveModulesRoot: 'src', // adjust based on your file structure
-        format: 'es',
-        entryFileNames: '[name].js'
-      }
-    }
   },
 	plugins: [sveltekit(),
 		//  webSocketServer
