7ee4551fbb7bfea53f33699cdb81faadf77236cf
Steve <ostylehouse@gmail.com>
2026-02-06 16:12:37 +1300

    F.P.NoPreviewing for a middle ground

--- ty/romount.sh
+++ ty/romount.sh
@@ -3,7 +3,7 @@ user=$(id -u)
 group=$(id -g)
 sudo mkdir -p /mnt/music-jamsend/-\ Steve
 sudo chown $user:$group /mnt/music-jamsend
-echo "/mnt/music /mnt/music-jamsend/-\ Steve none bind,ro 0 0" | sudo tee -a /etc/fstab
+echo "/mnt/music /mnt/music-jamsend/-\040Steve none bind,ro 0 0" | sudo tee -a /etc/fstab
 
 sudo mount /mnt/music-jamsend/-\ Steve
 
