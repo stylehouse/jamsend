e2eb381ad9884a620db350ebc248f86f26c094f6
Steve <ostylehouse@gmail.com>
2026-02-23 21:47:58 +1300

    no droids

--- droids.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# my ~/.ssh/config makes ds hostname=d user=s
-#  and over there s is in the group docker, is uid=1000
-# over there has also got to be:
-# ./music and ./chrome-profile
-#  and I had to chmod o+w them
-scp ty/bot.js docker-compose.droid.yml ds:
-ssh -L 7900:localhost:7900 ds docker compose up
