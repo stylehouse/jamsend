61fad8ff50dda61bf199826d5dc20a2f08be1dae
Steve <ostylehouse@gmail.com>
2026-04-07 17:19:32 +1200

    etc

--- src/lib/ghost/Machinery.svelte
+++ src/lib/ghost/Machinery.svelte
@@ -78,13 +78,26 @@
         }
 
 
+
+
+
+
+
+
+
+
+
+
         // from here down is studying the same resolve() bug
+        //  fixed in daf49c838fc30c7525a7bc2b315789fade0c45b1
+        //     C.resolve() rejects longtail of pairings via UNAMBIGUITY_THRESHOLD
+        // there is still a problem:
+        //  see: should %migrate it
 
 
 
 
-
-        o = w.i({ test: "C.resolve() test suite" })
+        o = w.i({ test: "C.resolve() leaf test suite" })
 
         const resolve_test = async (label, pattern_sc, first_strs, then_strs,DEV) => {
             const t = o.i({ resolve_test: label })
@@ -92,7 +105,7 @@
             first_strs.map(s => C.i({ ...pattern_sc, ...peel(s) }))
             const pairs = t.i({ pairs: 1 })
             if (DEV) C.coms = t.i({resolve_coms:1})
-            if (DEV) C.resolve = this.DEV_resolve
+            // if (DEV) C.resolve = this.DEV_resolve
             await C.replace(pattern_sc, async () => {
                 then_strs.map(s => C.i({ ...pattern_sc, ...peel(s) }))
             }, { pairs_fn: async (a, b) => {
@@ -201,8 +214,7 @@
         rh.i({tangiuy:1})
         if (captured_goners.length != 1 || !captured_goners[0].sc.the_leaf) o.i({heck:1})
 
-        // < should %migrate the o/the_leaf -> o/the_hand/the_leaf
-        //    the other test is not doing these other-level moves. why?
+        // we now get a %migrate for o/the_leaf -> o/the_hand/the_leaf
         await thefu('002 see goner, leaf into left',o)
 
         lh.empty()
@@ -210,7 +222,7 @@
         rh.i(laleaf)
 
 
-        // < should %migrate it. the other test was doing these sideways moves. why?
+        // < should %migrate it. Book:LeafJuggle was doing these sideways moves. why?
         await thefu('003 leaf moves left->right',o)
 
 
@@ -220,10 +232,10 @@
 
 
 
-        // < doesn't really explain anything until it takes on a quality of thefu()
-        //    that breaks %migrate showing the moving part...
-        //   it exposes no bugs yet
-        o = w.i({ test: "Selection: particle moves from root/* to root/container/*" })
+        // benign. doesn't really explain or expose anything.
+        //  this T%goners and Se_some_Migration() thing is novel.
+        //  ugly data except: Migration,n:wanderer,from_depth,to_depth:2
+        o = w.i({ test: "Selection: leaf moves from root/* to root/container/*" })
 
         const root = _C({ root_node: 1 })
         const leaf = root.i({ leaf: 1, name: 'wanderer' })
@@ -334,176 +346,6 @@
 
 
 
-
-
-//#endregion
-//#region DEV_resolve
-    async DEV_resolve(X:TheX,oldX:TheX,partial:TheN|null,q={}) {
-        if (!oldX?.z?.length) {
-            // everything is new
-            return (X.z||[]).map(n => [null,n])
-        }
-        // partial may be a set of old things we're replacing
-        //  if partial, there's other stuff in oldX we're not replacing
-        // but X is always the new stuff only
-        let partsof = (N:TheN) => {
-            return N.filter(n => !partial || partial.includes(n))
-        }
-        // debuggery
-        let coms = this.coms
-
-        // collect islands of same k+v
-        let Over = _C({})
-        Over.Xify()
-
-        let kv_iter = (X,fn) => {
-            Object.entries(X.k||{}).forEach(([k,kx]) => {
-                kx = kx as TheX
-                // eg k=nib, we're dividing nib=dir|blob
-                Object.entries(kx.v).forEach(([i,vx]) => {
-                    let v = kx.vs[i]
-                    fn(k,kx,v,vx)
-                })
-            })
-        }
-
-        // iterate the new k/v structure
-
-        kv_iter(X,(k,kx,v,vx) => {
-            // eg k=nib,v=dir
-
-            // expect some string property will be more disambiguating
-            // < ref matching. would be slower. do on the remainder?
-            let vtype = typeof v == 'object' ? 'ref' : 'string'
-            if (vtype == 'ref') return
-
-            // look for the same k/v
-            let oldvx = oldX.o_kv(k,v,{notwild:1})
-            if (!oldvx) return // none
-
-            if (!oldvx.z.length) throw `should always be some /$n`
-            let old_z = partsof(oldvx.z)
-            if (!old_z.length) {
-                // may share kv with the out-group
-                // < an odd occasion to study in testing
-                // console.warn("Perhaps your replace() pattern_sc doesn't match the new atoms?",{X,partial})
-                return
-            }
-
-            
-            vx.z.forEach((n:TheC,i:number) => {
-                // any neu%nib:dir could match any old%nib:dir
-                // via /$v:neu /$k/$v:stringval /$n=old
-                let nkvx = Over.X.i_v(n,null,'neu')
-                    .i_k(k).i_v(v,null)
-                old_z.forEach(oldn => {
-                    coms&&coms.i({candidate: keyser(n), via: `${k}:${v}`, oldn: keyser(oldn)})
-                    nkvx.i_z(oldn)
-                })
-            })
-        })
-
-
-
-        // /$v:neu /$k/$v:stringval /$n=old
-        Object.entries(Over.X.neu||{}).forEach(([i,_neux]) => {
-            let n = Over.X.neus[i]
-            let neux = _neux as TheX
-            kv_iter(neux,(k,kx,v,vx) => {
-                let possible = vx.z
-                // for %nib:dir x20 matching less than %name:veryunique x1
-                let unambiguity = 1 / possible.length
-                let newly_possible = X.i_k(k)?.i_v(v)?.z?.length
-                if (newly_possible) {
-                    // it wants to be unique in the past and future.
-                    unambiguity = (unambiguity + (1/newly_possible)) / 2
-                }
-                // /$neu /$ambiguity=0.234 /$n=old
-                let rated = neux.i_k(unambiguity,null,'unambiguity')
-                rated.z = [...vx.z]
-                // /$ambiguity=0.234 /$n=neu for ordering matches amognst all $neu
-                Over.X.i_k(unambiguity,n,'unambiguity')
-
-            })
-        })
-        let sort_unambiguity = (X) => {
-            return Object.keys(X.unambiguity||{}).sort().reverse()
-        }
-
-        // pairs of [oldn,n], eventual result
-        let pairs = []
-        // $neu dwindling to actual new items
-        let unfound:Array<TheC> = [...(X.z||[])]
-        // $oldn that become paired with a $neu
-        let claimed:Array<TheC> = []
-        let claim = (oldn,n) => {
-            pairs.push([oldn,n])
-            unfound = unfound.filter(m => m != n)
-            claimed.push(oldn)
-        }
-
-        // sort by unambiguity
-        let UNAMBIGUITY_THRESHOLD = 0.33
-        // /$ambiguity=0.234 /$n=neu/$ambiguity=0.234 /$n=old
-        let ratings = sort_unambiguity(Over.X)
-        ratings.forEach((unambiguity) => {
-            if (Number(unambiguity) < UNAMBIGUITY_THRESHOLD) return
-            let x = Over.X.unambiguity[unambiguity]
-            x.z.forEach((n:TheC) => {
-                if (!unfound.includes(n)) return
-
-                // /$v:neu
-                let neux = Over.X.i_v(n,null,'neu')
-                if (!neux?.k) throw `algo!?k`
-                let rated = neux.i_k(unambiguity,null,'unambiguity')
-                if (!rated.z.length) throw `algo!?z`
-                for (let oldn of rated.z) {
-                    if (claimed.includes(oldn)) continue
-                    coms&&coms.i({claiming: keyser(n), oldn: keyser(oldn), score: unambiguity})
-                    // < I fade out here. maybe with a better io notation...
-                    //   sorting through arrangements any more is...
-                    //    one of those has-been-done academic things
-                    // let oldnx = Over.X.i_v(oldn,n,'old')
-                    // if (oldnx.z.length > 1) coms&&coms.i({ambiguo:n,neu:keyser(n),oldn,old:keyser(oldn)})
-                    // < pile up neux/$maybe=oldn from many vx
-                    //    to union many takes on $oldn with decreasing pickiness
-                    //     depending on everyone else's contest...
-                    //    lots of permuting?
-                    // or just accept the first one?
-                    //  they are sorted for uniqueness, won't re-claim...
-                    if (q.resolve_strict) {
-                        // be more likely to drop and recreate things
-                        //  if any stringified value has changed
-                        let valuesOf = (n) => armap(v=>v+'',n.sc).join(',')
-                        if (valuesOf(n) != valuesOf(oldn)) {
-                            return
-                        }
-                    }
-                    claim(oldn,n)
-                    // once n is claimed, stop claiming oldn for it
-                    break
-                }
-            })
-        })
-
-        // what's left in X_before (that we are partial to replacing)
-        let gone = partsof(oldX.z||[])
-            .filter(oldn => !claimed.includes(oldn))
-        
-        // log it all
-        if (1 && coms) {
-            pairs.forEach(([oldn,n]) => coms.i({old:keyser(oldn),neu:keyser(n)}))
-            unfound.forEach((n) => coms.i({spawn:keyser(n)}))
-            gone.forEach((n) => coms.i({gone:keyser(n)}))
-        }
-
-        // new stuff
-        unfound.forEach((n) => pairs.push([null,n]))
-        // gone stuff
-        gone.forEach((oldn) => pairs.push([oldn,null]))
-
-        return pairs
-    },
 
 
 
