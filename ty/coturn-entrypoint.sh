6fdc152d32451a8340af2842e30d76e4624d160a
Steve <ostylehouse@gmail.com>
2026-04-30 00:17:06 +1200

    where certs, ips

--- ty/coturn-entrypoint.sh
+++ ty/coturn-entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 echo "$TLS_CERT" | base64 -d > /etc/ssl/coturn.crt
 echo "$TLS_KEY"  | base64 -d > /etc/ssl/coturn.key
 chmod 600 /etc/ssl/coturn.key
-sed "s|^external-ip=$|external-ip=$(curl -s -4 ifconfig.me)|" \
+
+PUBLIC_IP=$(curl -s -4 ifconfig.me)
+
+sed \
+    -e "s|^external-ip=$|external-ip=${PUBLIC_IP}|" \
+    -e "s|^relay-ip=.*|relay-ip=${PUBLIC_IP}|" \
     /etc/coturn/turnserver.conf > /tmp/turnserver.conf
+
 exec turnserver -c /tmp/turnserver.conf --log-file stdout --no-stdout-log
